ccc009417144703f93bbe71dc3312a4c613b173e
Nuzul Fikrie <nuzulfikrie@gmail.com>
2024-11-30 21:49:12 +0800

    modified seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\RoleAndPermissionSeeder;
+use Database\Seeders\CompanySeeder;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +16,9 @@ class DatabaseSeeder extends Seeder
 
         // Run other seeders
         $this->call([
+            RoleSeeder::class,
+            RoleAndPermissionSeeder::class,
+            UserSeeder::class,
             CompanySeeder::class,
         ]);
     }
